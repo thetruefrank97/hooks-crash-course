1b37468d4e633d7640af065677aea4e8a0cb76a3
fchavez <chavezfrancisco308@gmail.com>
2021-01-28 13:04:25 -0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Fragment,Component} from "react"
+import Navbar from "./Components/Layout/Navbar";
+import Users from "./Components/Users/Users";
+import UserItem from "./Components/Users/UserItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    // fah = () => {
+    //     return "Bars"
+    // }
+    render(){
+        // const name="John Doe";
+        // const loading = false;
+        // const foo = () => "Bar";
+        // const showName=true;
+
+        // if(loading){
+        //     return <h4>Loading...</h4>
+        // }
+
+        // const numbers = [1,2,3,4]
+
+        return (
+            <div className="App">
+                <Navbar title="Github Finder" icon="fab fa-github"/>
+                <div className="container">
+                    <Users/>
+                </div>
+                {/*{loading ? (*/}
+                {/*    <h4>Loading...</h4>*/}
+                {/*    ) : (*/}
+                {/*        <h1>Hello {showName ? name : null}</h1>*/}
+                {/*)}*/}
+                {/*/!*<h1 htmlFor="name">Hello {name}</h1>*!/*/}
+                {/*<h2> Hello {foo()}</h2>*/}
+                {/*<h3>Hello {this.fah()}</h3>*/}
+            </div>
+        );
+    }
 }
 
 export default App;
