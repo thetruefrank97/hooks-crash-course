f2e849fd172284edd0bce3a44b5ab8ab3b7854ca
fchavez <chavezfrancisco308@gmail.com>
2021-01-29 14:11:51 -0800

    Fourth commit

--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -1,9 +1,18 @@
-import React,{Component} from "react";
+import React,{Component,Fragment} from "react";
+import Spinner from "../Layout/Spinner";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 
 export class User extends Component{
     componentDidMount(){
         this.props.getUser(this.props.match.params.login);
+    }
+
+    static propTypes = {
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
     }
 
     render(){
@@ -19,17 +28,83 @@ export class User extends Component{
             following,
             public_repos,
             public_gists,
-            hireable
+            hireable,
+            company
         }= this.props.user;
 
         const{loading}=this.props;
 
-        return(
-            <div>
-                {name}
-            </div>
-        )
+        if(loading){
+            return <Spinner/>
+        }else{
+            return(
+                <Fragment>
+                    <Link to="/" className="btn btn-light">Back to Search</Link>
+                    Hireable: {" "}
+                    {hireable ? <i className="fas fa-check text-success"/> : <i className="fas
+                    fa-times-circle text-danger"/>}
+                    <div className="card grid-2">
+                        <div className="all-center">
+                            <img
+                                src={avatar_url}
+                                className="round-img"
+                                alt=""
+                                style={Avatar}
+                            />
+                            <h1>{name}</h1>
+                            <p>Location: {location}</p>
+                        </div>
+                        <div>
+                            {bio && <Fragment>
+                                <h3>Bio</h3>
+                                <p>{bio}</p>
+                            </Fragment>}
+                            <a href={html_url} className="btn btn-dark my-1">
+                                Visit Github Profile
+                            </a>
+                            <ul>
+                                <li>
+                                    {login && <Fragment>
+                                        <strong>Username:</strong> {login}
+                                    </Fragment>}
+                                </li>
+                                <li>
+                                    { company && <Fragment>
+                                        <strong>Company:</strong> {company}
+                                    </Fragment>}
+                                </li>
+                                <li>
+                                    {blog && <Fragment>
+                                        <strong>Website:</strong> {blog}
+                                    </Fragment>}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="card text-center">
+                            <div className="badge badge-primary">
+                                Followers: {followers}
+                            </div>
+                        <div className="badge badge-success">
+                            Following: {following}
+                        </div>
+                        <div className="badge badge-light">
+                            Public Repos: {public_repos}
+                        </div>
+                        <div className="badge badge-dark">
+                            Public Gists: {public_gists}
+                        </div>
+                    </div>
+                </Fragment>
+            )
+        }
+
+
     }
+}
+
+const Avatar = {
+    width: "150px"
 }
 
 export default User;
